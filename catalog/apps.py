039e8aa67932a4eee9700f40044e9c5977d5b24f
Patrick Rauer <29750318+patrickRauer@users.noreply.github.com>
2023-07-12 06:35:03 +0200

    adding default types

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
 
+def _create_default_entries():
+    from catalog.models import Type
+
+    for n in ['Exo-planet', 'Binary', 'QSO']:
+        Type.objects.get_or_create(name=n)
+
+
 class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
+
+    def ready(self):
+        _create_default_entries()
